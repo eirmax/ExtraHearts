83ccdbbd780c4f5a1088f3aa2b0afc332e5af73f
eirmax <eirmax8@gmail.com>
2025-06-17 20:58:05 +0700

    fix

---
+++ src/client/java/com/eirmax/mixin/client/ExtraHeartsClientMixin.java
@@ -0,0 +1,10 @@
+package com.eirmax.mixin.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class ExtraHeartsClientMixin implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
